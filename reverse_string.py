17e1cc8295f6a06a794aa0ac0fe4f9a3329e0331
Goutham-0255 <gouthamf01@gmail.com>
2025-01-23 05:30:04 +0530

    commented

--- reverse_string.py
+++ reverse_string.py
@@ -1,12 +1,13 @@
-def find_first_repeated_number(sequence):
-    # Iterate through the sequence, starting from the second element
-    for j in range(1, len(sequence)):
-        # Check all previous elements for a match with the current element
-        for i in range(0, j):
-            if sequence[j] == sequence[i]:  # Found a repeated number
-                print(sequence[j])
+def first_repeated_number(num):
+    for i in range(0, len(num)):
+
+        #innner loop or pointer that commpare with i if the value is repeated
+        for j in range(i+1, len(num)):
+
+            #if repreated it return the value
+            if num[i] == num[j]:  
+                print(num[i])
                 return
-    # If no repeated number is found after checking all elements
     print("No repeated number found.")
 
-find_first_repeated_number([1, 2, 3, 1, 3])
+first_repeated_number([1, 2, 3, 1, 3])
