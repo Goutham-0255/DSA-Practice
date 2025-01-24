d67b6c488c3777729f83331626b64e9934cd0aee
Goutham-0255 <gouthamf01@gmail.com>
2025-01-24 05:40:10 +0530

    array searching

---
+++ array_search.py
@@ -0,0 +1,7 @@
+def search_element(array, x):
+
+    for i in range(len(array)):
+            # CHECK IF THE ARRAY VALUE AND X IS EWUAL AND RETURN IT
+            if array[i] == x:
+                return i
+    
