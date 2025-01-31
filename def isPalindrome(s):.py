cf2895d9ee3e5ccc1a2ed98bc032e5bcd768955b
Goutham-0255 <gouthamf01@gmail.com>
2025-01-31 08:54:11 +0530

    palindrome

---
+++ def isPalindrome(s):.py
@@ -0,0 +1,15 @@
+def isPalindrome(s):
+
+
+    s = s.lower()
+
+
+    nonalnum_char = ""
+
+
+    for char in s:
+        if char.isalnum():
+            nonalnum_char += char
+    return nonalnum_char == nonalnum_char[::-1]
+
+print(isPalindrome(121))
