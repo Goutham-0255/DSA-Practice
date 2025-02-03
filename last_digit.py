7d700c97bf9a6dde4b1b22108c4b5ef494b8d395
Goutham-0255 <gouthamf01@gmail.com>
2025-02-03 17:36:16 -0500

    Last digit

---
+++ last_digit.py
@@ -0,0 +1,7 @@
+def Last_digit(num):
+
+    # modulus by 10 will give the remainder of the number (that number is the last digit) 
+    result = num % 10 
+
+    return result
+print(Last_digit(120))
