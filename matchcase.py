cd2f8918b87c964609bc1be1198ad5736eaee5a0
Goutham-0255 <gouthamf01@gmail.com>
2025-02-05 17:38:29 -0500

    Match case

---
+++ matchcase.py
@@ -0,0 +1,23 @@
+import math
+
+def switchCase(choice, arr):
+        
+        match choice:
+            case 1:
+
+
+                # AREA of circle formula (pi R^2)
+                return math.pi*(arr[0]**2)
+            case 2:
+
+                # area of ractangle formula (L*B)
+                return arr[0]*arr[1]
+            case 3:
+                return 'invalid number'
+             
+result1 = switchCase(1, [5]) 
+result2 = switchCase(2, [5, 10])  
+result3 = switchCase(3, [])
+
+
+print(result1)
