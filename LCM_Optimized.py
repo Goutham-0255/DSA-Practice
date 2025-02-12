d961e597185e94e0741517200d3874e86225ccab
Goutham-0255 <gouthamf01@gmail.com>
2025-02-12 16:27:47 -0500

    GCD optimized

--- LCM_Optimized.py
+++ LCM_Optimized.py
@@ -1,10 +1,6 @@
 def gcd(a,b):
     if a == 0 and b == 0:
         return 'undefined'
-    if a == 0:
-        return b
-    if b == 0:
-        return a
     
     while b>0:
         a,b = b,a % b
