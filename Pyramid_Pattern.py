154f5a73daadbefdccded39d621b977a64d2992a
Goutham-0255 <gouthamf01@gmail.com>
2025-02-14 17:16:44 -0500

    Pyramid Pattern

--- Pyramid_Pattern.py
+++ Pyramid_Pattern.py
@@ -8,9 +8,9 @@ def pyramid(n):
         for j in range(0,2*i+1,1):
             print("*",end="")
 
-        #print space
-        for s in range(0,n-i-1,1):
-            print(" ",end="")
+        #print space not need but did it understand when practice to understand
+        '''for s in range(0,n-i-1,1):
+            print(" ",end="")'''
         print()
 
 
